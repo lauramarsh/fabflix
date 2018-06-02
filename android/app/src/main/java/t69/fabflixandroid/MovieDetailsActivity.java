277c4468def4f842f409cf71b8930bced8b4cc6c
knquintana96 <knquinta@uci.edu>
2018-06-01 23:30:48 -0700

    movie details, search activity, other stuff

---
+++ android/app/src/main/java/t69/fabflixandroid/MovieDetailsActivity.java
@@ -0,0 +1,32 @@
+package t69.fabflixandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.HashMap;
+import android.widget.TextView;
+
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+
+    TextView textView;
+    HashMap<String, String> details = new HashMap<String, String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        Bundle bundle = getIntent().getExtras();
+        details = (HashMap<String, String>) bundle.get("content");
+        populateDetailsMap(details);
+    }
+
+    private void populateDetailsMap(HashMap<String, String> hm) {
+        String detailString = "Title: " + hm.get("title")
+                + "\n\nYear: " + hm.get("year")
+                + "\n\nDirector: " + hm.get("director")
+                + "\n\nGenres: " + hm.get("genres")
+                + "\n\nStars: " + hm.get("stars");
+}
+
--- android/app/src/main/java/t69/fabflixandroid/SearchActivity.java
+++ android/app/src/main/java/t69/fabflixandroid/SearchActivity.java
@@ -1,29 +1,42 @@
 package t69.fabflixandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+import android.content.Intent;
+
+
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText movie;
+    Button   search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        movie = (EditText) findViewById(R.id.movie);
+        search = (Button)findViewById(R.id.search);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                performSearch(view, movie.getText().toString());
             }
         });
+
     }
 
+    public void performSearch(View view, String movie) {
+        Intent searchResults = new Intent(this, ResultsActivity.class);
+        searchResults.putExtra("movie", movie);
+        startActivity(searchResults);
+    }
 }
