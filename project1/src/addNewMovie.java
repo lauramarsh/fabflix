7755c54888acc4559a80fa71a9244cc26c652a1a
Laura Marsh <lmarsh@tenantbase.com>
2018-06-11 18:33:27 -0700

    Changed write queries from moviedb to Masterdb

--- project1/src/addNewMovie.java
+++ project1/src/addNewMovie.java
@@ -54,7 +54,7 @@ public class addNewMovie extends HttpServlet {
                 out.println("envCtx is NULL");
 
             // Look up our data source
-            DataSource ds = (DataSource) envCtx.lookup("jdbc/MovieDb");
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/MasterDb");
             
             if (ds == null)
                 out.println("ds is null.");
